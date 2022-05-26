d40d47123ff046e48bef50140158e12f93aae58a
dabiko <dabiko.blaise@gmail.com>
2022-05-26 22:43:55 +0100

    configuring firebase and installing firebase packages

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDqKeSf5WYsnB8i4VbFeaakRaDNXFhZrfY",
-    authDomain: "challenge-78fda.firebaseapp.com",
-    projectId: "challenge-78fda",
-    storageBucket: "challenge-78fda.appspot.com",
-    messagingSenderId: "88543556834",
-    appId: "1:88543556834:web:0c77046600fbfdb7001abb",
-    measurementId: "G-LZGQC1RJ8L"
-  };
+  apiKey: "AIzaSyDqKeSf5WYsnB8i4VbFeaakRaDNXFhZrfY",
+  authDomain: "challenge-78fda.firebaseapp.com",
+  projectId: "challenge-78fda",
+  storageBucket: "challenge-78fda.appspot.com",
+  messagingSenderId: "88543556834",
+  appId: "1:88543556834:web:0c77046600fbfdb7001abb",
+  measurementId: "G-LZGQC1RJ8L"
+};
