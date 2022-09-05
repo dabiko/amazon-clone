b568b85f7680937756969238fb89a9bd8538af6f
dabiko <dabiko.blaise@gmail.com>
2022-09-05 09:34:58 +0100

    removing amazon public Key

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqKeSf5WYsnB8i4VbFeaakRaDNXFhZrfY",
+  apiKey: "",
   authDomain: "challenge-78fda.firebaseapp.com",
   projectId: "challenge-78fda",
   storageBucket: "challenge-78fda.appspot.com",
   messagingSenderId: "88543556834",
-  appId: "1:88543556834:web:0c77046600fbfdb7001abb",
-  measurementId: "G-LZGQC1RJ8L"
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
